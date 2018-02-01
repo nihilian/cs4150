5aa3325f2d0b42ee5ae046e179dc899756b9bdd2
Benjamin Allred <u1090524@utah.edu>
2018-01-31 20:14:56 -0700

    GalaxyQuest

---
+++ GalaxyQuest/GalaxyQuest/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kattis.IO;
+
+namespace GalaxyQuest
+{
+    class Program
+    {
+        static long dSqr;
+        private class Star
+        {
+            public long X { private set; get; }
+            public long Y { private set; get; }
+            public Star(long x, long y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner();
+            long d = scanner.NextLong();
+            long k = scanner.NextLong();
+            dSqr = d * d;
+            List<Star> stars = new List<Star>();
+
+            for (int i = 0; i < k; i++)
+            {
+                long x = scanner.NextLong();
+                long y = scanner.NextLong();
+                stars.Add(new Star(x, y));
+            }
+
+            long majorityCount = 0;
+            FindMajority(stars, ref majorityCount);
+            if (majorityCount == 0)
+                Console.WriteLine("NO");
+            else
+                Console.WriteLine(majorityCount);
+            Console.Read();
+        }
+
+        private static Star FindMajority(List<Star> stars, ref long count)
+        {
+            if (stars.Count == 0)
+            {
+                count = 0;
+                return null;
+            }
+                
+            else if (stars.Count == 1)
+            {
+                count = 1;
+                return stars[0];
+            }
+                
+            else
+            {
+                List<Star> starsPrime = new List<Star>();
+
+                for (int i = 0; i < stars.Count-1; i += 2)
+                {
+                    if (InSameGalaxy(stars[i], stars[i + 1]))
+                    {
+                        starsPrime.Add(stars[i]);
+                    }    
+                }
+
+                Star x = FindMajority(starsPrime, ref count);
+                
+                if (x == null)
+                {
+                    if (stars.Count % 2 == 1)
+                    {
+                        count = 1;
+                        for (int i = 0; i < stars.Count-1; i++)
+                        {
+                            if (InSameGalaxy(stars[i], stars[stars.Count - 1]))
+                                count++;
+                        }
+                        if (count > stars.Count / 2)
+                        {
+                            return stars[stars.Count - 1];
+                        }   
+                        else
+                        {
+                            count = 0;
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                        return null;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                    for (int i = 0; i < stars.Count; i++)
+                    {
+                        if (InSameGalaxy(stars[i], x))
+                            count++;
+                    }
+                    if (count > stars.Count / 2)
+                    {
+                        return x;
+                    }
+                    else
+                    {
+                        count = 0;
+                        return null;
+                    }
+                }
+            }
+        }
+
+        private static bool InSameGalaxy(Star a, Star b)
+        {
+            long _dSqr = (a.X - b.X)*(a.X - b.X) + (a.Y - b.Y)*(a.Y - b.Y);
+            if (_dSqr > dSqr)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ Tests/Tests/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 7, 8, 9, 14 };
+            int[] B = { 2, 5, 6, 22 };
+            int[] M = { 2, 5, 6, 7, 8, 9, 14, 22 };
+
+            for( int k = 0; k < 8; k++)
+                Console.WriteLine(select(A, B, k));
+            Console.Read();
+        }
+
+        // A and B are each sorted into ascending order, and 0 <= k < |A|+|B| 
+        // Returns the element that would be stored at index k if A and B were
+        // combined into a single array that was sorted into ascending order.
+        static public int select(int[] A, int[] B, int k)
+        {
+            return select(A, 0, A.Length - 1, B, 0, B.Length - 1, k);
+        }
+
+
+        static private int select(int[] A, int loA, int hiA, int[] B, int loB, int hiB, int k)
+        {
+            // A[loA..hiA] is empty
+            if (hiA < loA)
+                return B[k - loA];
+            // B[loB..hiB] is empty
+            if (hiB < loB)
+                return A[k - loB];
+            // Get the midpoints of A[loA..hiA] and B[loB..hiB]
+            int i = (loA + hiA) / 2;
+            int j = (loB + hiB) / 2;
+            // Figure out which one of four cases apply
+            if (A[i] > B[j])
+            {
+                if (k <= i + j)
+                    return select(A, loA, i-1, B, loB, hiB, k);
+                else
+                    return select(A, loA, hiA, B, j+1, hiB, k);
+            }
+
+            else
+            {
+                if (k <= i + j)
+                    return select(A, loA, hiA, B, loB, j-1, k);
+                else
+                    return select(A, i+1, hiA, B, loB, hiB, k);
+            }
+
+        }
+
+    }
+}
